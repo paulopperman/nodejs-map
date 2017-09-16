136e50be392660816ab5508f8c70634f07887cf6
paulopperman <paul.opperman@gmail.com>
2017-09-16 14:34:47 -0400

    step 4.2 - dbquery

--- server/api.js
+++ server/api.js
@@ -1,3 +1,23 @@
 /**
  * API Routes Module
  */
+
+const Router = require('koa-router')
+const database = require('./database')
+const cache = require('./cache')
+const joi = require('joi')
+const validate = require('koa-joi-validate')
+
+const router = new Router()
+
+//Hello world test endpoint
+router.get('/hello', async ctx => {
+  ctx.body = "Hello World"
+})
+
+// Get time from database
+router.get('/time', async ctx => {
+  const result = await database.queryTime()
+  ctx.body = result
+})
+module.exports = router
--- server/database.js
+++ server/database.js
@@ -1,3 +1,23 @@
 /**
  * Postgres DB Module
  */
+
+const postgres = require('pg')
+const log = require('./logger')
+const connectionString = process.env.DATABASE_URL
+
+// initialize postgres client
+const client = new postgres.Client({ connectionString })
+
+// connect to the database
+client.connect().then(() => {
+  log.info(`Connected To ${client.database} at ${client.host}:${client.port}`)
+}).catch(log.error)
+
+module.exports = {
+  /** Query the current time **/
+  queryTime: async () => {
+    const result = await client.query('SELECT NOW() as now')
+    return result.rows[0]
+  }
+}
