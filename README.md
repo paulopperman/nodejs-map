ebf76fa3578c65a07857c9badeeaa8350d34e501
paulopperman <paul.opperman@gmail.com>
2017-09-16 16:30:37 -0400

    add geojson queries - step 5

--- README.md
+++ README.md
@@ -17,13 +17,15 @@ The only extra step is adding a `.env` file in order to properly initialize the 
 Here's an example `.env` file with sensible defaults for local development -
 ```
 PORT=5000
-DATABASE_URL=postgres://patrick@localhost:5432/atlas_of_thrones?ssl=false
+DATABASE_URL=postgres://thrones:<pwd>@localhost:5432/atlas_of_thrones?ssl=false
 REDIS_HOST=localhost
 REDIS_PORT=6379
 CORS_ORIGIN=http://localhost:8080
 ```
 
-You'll need to change the username in the DATABASE_URL entry to match your Postgres user credentials. Unless your name is "Patrick", that is, in which case it might already be fine.
+You'll need to change the username in the DATABASE_URL entry to match your Postgres user credentials.
 
+If having problems making queries, connect to the db and run:
+`GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO thrones;`
 
 Run `npm run local` to start the API server on `localhost:5000`, and to build/watch/serve the frontend code from `localhost:8080`.
--- server/api.js
+++ server/api.js
@@ -20,4 +20,36 @@ router.get('/time', async ctx => {
   const result = await database.queryTime()
   ctx.body = result
 })
+
+// get locations as geojson
+router.get('/locations/:type', async ctx => {
+  const type = ctx.params.type
+  const results = await database.getLocations(type)
+  if (results.length === 0) {ctx.throw(404)}
+
+  // add row metadata as geojson properties
+  const locations = results.map((row) => {
+    let geojson = JSON.parse(row.st_asgeojson)
+    geojson.properties = { name: row.name, type: row.type, id: row.gid}
+    return geojson
+  })
+
+  ctx.body = locations
+})
+
+// Return the boundary geojson for all kingdoms
+router.get('/kingdoms', async ctx => {
+  const results = await database.getKingdomBoundaries()
+  if (results.length === 0) { ctx.throw(404) }
+
+  // add row metatdata as geojson properties
+  const boundaries = results.map((row) => {
+    let geojson = JSON.parse(row.st_asgeojson)
+    geojson.properties = {name: row.name, id: row.gid}
+    return geojson
+  })
+
+  ctx.body = boundaries
+})
+
 module.exports = router
